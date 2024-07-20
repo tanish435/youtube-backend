40a9c5ae7d6b8a81b3c49557851c477c2268a4a9
tanish435 <tanishrai@gmail.com>
2024-07-20 18:12:13 +0530

    utils code

--- src/index.js
+++ src/index.js
@@ -5,6 +5,19 @@ import connectDB from "./db/index.js";
 dotenv.config({path: './env'})
 
 connectDB()
+.then(() => {
+    app.on("error", (error) => {
+        console.log("ERR: ", error);
+        throw error
+    })
+    
+    app.listen(process.env.PORT || 8000, () => {
+        console.log(`Server is running on port : ${process.env.PORT}`);
+    })
+})
+.catch((err) => {
+    console.log("MONDO db connection failed: !!!", err);
+})
 
 
 
