230caa138638e3b390f07e13e780c83a1ff7af40
tanish435 <tanishrai@gmail.com>
2024-08-12 16:43:03 +0530

    Add comment route to app.js

--- src/app.js
+++ src/app.js
@@ -17,9 +17,11 @@ app.use(cookieParser())
 
 // routes import
 import userRouter from './routes/user.routes.js'
+import commentRouter from './routes/comment.routes.js'
 
 // routes declaration
 app.use("/api/v1/users", userRouter) 
+app.use("/api/v1/comments", commentRouter)
 // http://localhost:8080/api/v1/users/register
 
 export { app }
